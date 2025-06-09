5bb60c0ac44479cb08ca13f9c9e98cc98f658c86
Milk Wee <1140484715@qq.com>
2025-06-09 09:23:19 +0800

    断言和测试用例2

---
+++ maven-project01/src/main/java/com/itheima/UserService.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class UserService {
+    public Integer getAge(String number) {
+        return 18;
+    }
+
+    public String getGender(String number) {
+        return "男";
+    }
+}
